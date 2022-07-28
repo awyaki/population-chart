96c4f90630f7860a763b9fcbf1c78026b039f11a
awyaki <awyakitaka@gmail.com>
2022-07-28 22:51:02 +0900

    feat: add the view of check boxes for prefectures to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+import { Prefectures } from "@/types";
+import { useCheckboxList } from "@/hooks";
+import { getAllPrefectures } from "@/lib";
 
-const Home: NextPage = () => {
-  return <></>;
+type HomePageProps = {
+  prefectures: Prefectures;
+};
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  const prefs: Prefectures = (await getAllPrefectures()).data.result.map(
+    ({ prefCode, prefName }) => ({ id: prefCode, name: prefName })
+  );
+
+  return {
+    props: {
+      prefectures: prefs,
+    },
+  };
+};
+
+const Home: NextPage<HomePageProps> = ({ prefectures }) => {
+  const [checkedIds, renderCheckboxList] = useCheckboxList(prefectures);
+  return <>{renderCheckboxList()}</>;
 };
 
 export default Home;

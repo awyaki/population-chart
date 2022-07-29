b234baac24f8b925351b8b3b08d6e4f799897df2
awyaki <awyakitaka@gmail.com>
2022-07-29 12:36:46 +0900

    fix: pass values of a correct type(number) of id for CheckboxList props

--- src/components/CheckboxList/CheckboxList.stories.tsx
+++ src/components/CheckboxList/CheckboxList.stories.tsx
@@ -12,17 +12,17 @@ export const ListOfPrefectures: ComponentStoryObj<typeof CheckboxList> = {
     title: "都道府県",
     data: [
       {
-        id: "1",
+        id: 1,
         isChecked: true,
         value: "東京都",
       },
       {
-        id: "2",
+        id: 2,
         isChecked: true,
         value: "大阪府",
       },
       {
-        id: "3",
+        id: 3,
         isChecked: false,
         value: "兵庫県",
       },

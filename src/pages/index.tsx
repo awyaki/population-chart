33131ceab0ed24362e7a7046e94691c55093f54a
awyaki <awyakitaka@gmail.com>
2022-07-29 17:35:08 +0900

    feat: add allPopulation property to value returned by getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 import type { NextPage, GetStaticProps } from "next";
-import { Prefecture } from "@/types";
+import { Prefecture, Population } from "@/types";
 import { useCheckboxList } from "@/hooks";
-import { getAllPrefectures } from "@/lib";
+import { getAllPrefectures, getPopulation } from "@/lib";
 
 type HomePageProps = {
   prefectures: Prefecture[];
+  allPopulation: Population[];
 };
 
 export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
   const prefs = (await getAllPrefectures()).data.result;
-
+  const allPopulation = await getPopulation(prefs);
   return {
     props: {
       prefectures: prefs,
+      allPopulation: allPopulation,
     },
   };
 };

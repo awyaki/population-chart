c124cfb5c589c9915f16748f4350b98cc3f40c5e
awyaki <awyakitaka@gmail.com>
2022-07-29 10:01:49 +0900

    feat: add a type of RESASPopulation

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,14 @@ export type RESASPrefectures = {
   message: null;
   result: { prefCode: number; prefName: string }[];
 };
+
+export type RESASPopulation = {
+  message: null;
+  result: {
+    boundaryYear: number;
+    data: {
+      label: string;
+      data: { year: number; value: number; rate?: number }[];
+    }[];
+  };
+};

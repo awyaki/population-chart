e22d901e46ca609fb606b11ac5ef1e6e7a84dd6f
awyaki <awyakitaka@gmail.com>
2022-07-29 13:56:32 +0900

    feat: add a new hook useCheckboxList

---
+++ src/hooks/useCheckboxList.tsx
@@ -0,0 +1,34 @@
+import { useState, ComponentProps, useCallback } from "react";
+import { CheckboxList } from "@/components";
+import { Prefectures } from "@/types";
+
+export const useCheckboxList = (
+  prefectures: Prefectures
+): [number[], () => JSX.Element] => {
+  const [checkedIds, setCheckedIds] = useState<number[]>([]);
+
+  const handleClick = useCallback((id: number) => {
+    setCheckedIds((prev) => {
+      const index = prev.findIndex((checkedId) => checkedId === id);
+      return index === -1
+        ? prev.concat(id) // If id isn't in prev, add the id.
+        : prev.slice(0, index).concat(prev.slice(index + 1)); // If id is in prev,  remove the id.
+    });
+  }, []);
+
+  const renderCheckboxListForPrefectures = useCallback(() => {
+    const data: ComponentProps<typeof CheckboxList>["data"] = prefectures.map(
+      ({ id, name }) => {
+        const isChecked = checkedIds.includes(id);
+        return {
+          id,
+          isChecked,
+          value: name,
+        };
+      }
+    );
+    return <CheckboxList title="都道府県" data={data} />;
+  }, [checkedIds, prefectures]);
+
+  return [checkedIds, renderCheckboxListForPrefectures];
+};

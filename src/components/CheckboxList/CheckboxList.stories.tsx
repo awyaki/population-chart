282ac07355d420820853357a4c7854401f2f52d4
awyaki <awyakitaka@gmail.com>
2022-07-29 11:03:29 +0900

    fix: add id prop to CheckboxList

--- src/components/CheckboxList/CheckboxList.stories.tsx
+++ src/components/CheckboxList/CheckboxList.stories.tsx
@@ -12,14 +12,17 @@ export const ListOfPrefectures: ComponentStoryObj<typeof CheckboxList> = {
     title: "都道府県",
     data: [
       {
+        id: "1",
         isChecked: true,
         value: "東京都",
       },
       {
+        id: "2",
         isChecked: true,
         value: "大阪府",
       },
       {
+        id: "3",
         isChecked: false,
         value: "兵庫県",
       },

590b4898c79bf126856de1e7d4701c0a540ae993
awyaki <awyakitaka@gmail.com>
2022-07-31 13:58:25 +0900

    chore: add some font sizes for responsive design

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -16,4 +16,12 @@ export const {
     lg: "(min-width: 992px)",
     xl: "(min-width: 1200px)",
   },
+  theme: {
+    fontSizes: {
+      sm: "clamp(1rem, 0.75rem + 1.5vw, 2rem)",
+      md: "clamp(1.3rem, 1.05rem + 1.5vw, 2.3rem)",
+      lg: "clamp(1.5rem, 1.25rem + 1.5vw, 2.5rem)",
+      xl: "clamp(1.7rem, 1.45rem + 1.5vw, 2.7rem)",
+    },
+  },
 });

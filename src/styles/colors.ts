d23e9769b6212a18b9ff19630b061ceac1857bbc
awyaki <awyakitaka@gmail.com>
2022-08-01 19:31:29 +0900

    chore: add some colors
    * These added colors are referenced to CUD color set guidebook 2018.
    Use the first accent colors set in p.7.

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+/*
+ * This color set is based on "CUD color set guidebook 2018"
+ * */
+export const colors = {
+  accent: {
+    orange: "rgb(246, 170, 0)",
+    yellow: "rgb(255, 241, 0)",
+    green: "rgb(3, 175, 122)",
+    blue: "rgb(0, 90, 255)",
+    skyBlue: "rgb(77, 196, 255)",
+    brown: "rgb(128, 64, 0)",
+  },
+};

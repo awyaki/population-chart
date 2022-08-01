d12bf72778a477fd1d01fa0da16c3b0e1541d450
awyaki <awyakitaka@gmail.com>
2022-08-01 20:46:31 +0900

    chore: Include stories directory on a pattern to glob story files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 const path = require("path");
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  stories: [
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",

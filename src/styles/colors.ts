ce0c7bca33821002c267cec9bb6f4eeddecebb23
awyaki <awyakitaka@gmail.com>
2022-08-01 19:56:17 +0900

    chore: remove yellow from color set
    * Yellow isn't readable on a white screen, so remove yellow.

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,7 +4,6 @@
 export const colors = {
   accent: {
     orange: "rgb(246, 170, 0)",
-    yellow: "rgb(255, 241, 0)",
     green: "rgb(3, 175, 122)",
     blue: "rgb(0, 90, 255)",
     skyBlue: "rgb(77, 196, 255)",
